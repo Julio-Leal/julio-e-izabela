ba825a14f63f5c4048db9a686d58e1311aff0115
Júlio André Cavalcante Leal <cavalcantejulio522@gmail.com>
2025-08-13 23:04:18 -0300

    cria o calculo de tempo para noivado e namoro

--- src/script.js
+++ src/script.js
@@ -36,3 +36,33 @@ btnPrev.addEventListener('click', () => prevSlider());
 btnNext.addEventListener('click', () => nextSlider());
 
 console.log(slider)
+
+function calcularTempoNamoro () {
+    let hoje = new Date();
+    let dataInincio = new Date("2022-06-02");
+
+    // Diferença em milissegundos
+    let diferencaMs = hoje - dataInincio;
+
+    // Converte milissegundos para dias
+    let anos = Math.floor(diferencaMs / (1000*60*60*24*365)).toFixed(0);
+    
+    document.getElementById("tempo-namoro").textContent = anos;
+}
+
+
+function calcularTempoNoivado () {
+    let hoje = new Date();
+    let dataInincio = new Date("2024-06-02");
+
+    // Diferença em milissegundos
+    let diferencaMs = hoje - dataInincio;
+
+    // Converte milissegundos para dias
+    let anos = Math.floor(diferencaMs / (1000*60*60*24*365)).toFixed(0);
+    
+    document.getElementById("tempo-noivado").textContent = anos;
+}
+
+window.onload = calcularTempoNoivado;
+window.onload = calcularTempoNamoro;
